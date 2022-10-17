c1cedae34011cc5e2a5ef600c95deee893a5877d
BKB <blainekb94@gmail.com>
2022-10-17 02:41:24 -0500

    added models for Comment, Post, and User

---
+++ models/Comment.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config.js');
+
+class Comment extends Model { }
+Comment.init({
+  body: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+    sequelize,
+});
+
+module.exports = Comment;
---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config.js');
+
+class Post extends Model {}
+Post.init({
+  title:DataTypes.STRING,
+  text:DataTypes.STRING,
+}, {
+    sequelize,
+});
+
+module.exports = Post;
